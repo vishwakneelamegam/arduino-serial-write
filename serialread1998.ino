0f73daa954fd65475c992c26499a955c90f4a2ec
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2019-04-07 09:22:00 +0530

    Add files via upload

---
+++ serialread1998.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  String a;
+  if(Serial.available()>0){
+    a=Serial.readString();
+    a.trim();
+    Serial.println(a);
+    if(a=="apple")
+    {
+      Serial.println("SUCCESS");
+      }
+    }
+    if(Serial.available()<=0){}
+}
